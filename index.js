76693d60ac953dcffb8022eeaab8fa631aa6051a
RockyWearsAHat <alexwaldmann2004@gmail.com>
2023-12-09 05:59:51 -0700

    New Branch branch2

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ const executeShellCommand = async (command = "", verifyUserCommand = false) => {
       // console.error(e);
       return null;
     }
-    //Testing comment
 
     /*IF THIS COMMAND IS A VERIFY COMMAND, ERROR WILL BE THROWN BUT IS EXPECTED */
     let res;
